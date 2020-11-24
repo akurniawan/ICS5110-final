21dae9a10b69a7c1d38bf21bd491a14e1e0b83e7
akurniawan <akurniawan.cs@gmail.com>
2020-11-24 08:41:35 +0100

    update results from hyperparameter tuning

--- json_converter.py
+++ json_converter.py
@@ -2,9 +2,9 @@ import csv
 import json
 
 f = csv.writer(open("test.csv", "w", newline=""), delimiter="\t")
-f.writerow(["review_id", "stars", "useful", "text"])
+f.writerow(["review_id", "stars", "useful", "date", "text"])
 
 with open("yelp_academic_dataset_review.json", "r") as jsonf:
     for line in jsonf:
         x = json.loads(line)
-        f.writerow([x["review_id"], x["stars"], x["useful"], x["text"]])
+        f.writerow([x["review_id"], x["stars"], x["useful"], x["date"], x["text"]])
